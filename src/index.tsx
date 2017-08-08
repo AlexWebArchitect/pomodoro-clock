86775529e0064d58fcbbf78342eb49edb0a08847
AlexWebArchitect <alex.web.architect@gmail.com>
2017-08-08 13:57:43 +0300

    add working counter

--- src/index.tsx
+++ src/index.tsx
@@ -4,46 +4,22 @@ import './index.css';
 
 // ========================================
 
-interface DigitProps {
-  value: string;
-  onClick(): void;
+interface CounterProps {
+  name: string;
+  counter: number;
+  onClick(name: string, value: string): void;
 }
-interface DigitState { }
+interface CounterState { }
 
-class Digit extends React.Component<DigitProps, DigitState> {
-  render() {
-    return (
-      <button className="digit" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
-}
-
-interface BoardProps {
-  onClick(value: string): void;
-}
-interface BoardState { }
-
-class Board extends React.Component<BoardProps, BoardState>  {
-  renderDigit(val: string) {
-    return <Digit value={val} onClick={() => this.props.onClick(val)} />;
-  }
-
+class Counter extends React.Component<CounterProps, CounterState>  {
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderDigit('7')}{this.renderDigit('8')}{this.renderDigit('9')}{this.renderDigit('*')}
-        </div>
-        <div className="board-row">
-          {this.renderDigit('4')}{this.renderDigit('5')}{this.renderDigit('6')}{this.renderDigit('-')}
-        </div>
-        <div className="board-row">
-          {this.renderDigit('1')}{this.renderDigit('2')}{this.renderDigit('3')}{this.renderDigit('+')}
-        </div>
-        <div className="board-row">
-          {this.renderDigit('C')}{this.renderDigit('0')}{this.renderDigit('.')}{this.renderDigit('/')}
+        {this.props.name} LENGTH
+        <div>
+          <button onClick={() => this.props.onClick(this.props.name, '-')}>-</button>
+          {this.props.counter}
+          <button onClick={() => this.props.onClick(this.props.name, '+')}>+</button>
         </div>
       </div>
     );
@@ -52,100 +28,66 @@ class Board extends React.Component<BoardProps, BoardState>  {
 
 interface Props { }
 interface State {
-  display: string;
-  pushed: Array<string>;
+  mode: string;
+  break: number;
+  session: number;
 }
 
-class Calculator extends React.Component<Props, State> {
+class PomodoroClock extends React.Component<Props, State> {
   constructor() {
     super();
     this.state = {
-      display: '0',
-      pushed: ['']
+      mode: 'Start',
+      break: 1,
+      session: 1
     };
   }
 
-  handleClick(i: string): void {
-    let pushed = this.state.pushed;
-    let l: any = pushed[pushed.length - 1];
-    let I: any = i;
-    if (!isNaN(l * 1) && (!isNaN(I * 1) || I === '.')) {
-      pushed[pushed.length - 1] = l + i;
-      this.setState({
-        pushed: pushed,
-        display: pushed[pushed.length - 1]
-      });
-    } else if (i === '*' || i === '-' || i === '+' || i === '/') {
-      pushed.push(i);
-      this.setState({
-        pushed: pushed
-      });
-    } else if (isNaN(l * 1) && !isNaN(I * 1)) {
-      pushed.push(i);
-      this.setState({
-        pushed: pushed,
-        display: pushed[pushed.length - 1]
-      });
-    } else if (i === 'C') {
-        this.setState({
-          pushed: [''],
-          display: '0'
-        });
+  handleClick(n: string, i: string): void {
+    let counter: number;
+    switch (n) {
+      case 'BREAK':
+        counter = this.state.break;
+        if ( i === '+') {
+          this.setState({
+            break: counter + 1
+          });
+        } else {
+          this.setState({
+            break: counter - 1 
+          });
+        }
+        break;
+      case 'SESSION':
+        counter = this.state.session;
+        if ( i === '+') {
+          this.setState({
+            session: counter + 1
+          });
+        } else {
+          this.setState({
+            session: counter - 1 
+          });
+        }
+        break;
+      default:
+        break;
     }
   }
 
-  calculate(): void {
-    let pushed: Array<any> = this.state.pushed;
-    for (var z = 1; z < pushed.length; z = z + 2) {
-      switch (pushed[z]) {
-        case '*':
-          pushed[z + 1] = (pushed[z - 1] * 1) * (pushed[z + 1] * 1);
-          this.setState({
-            pushed: pushed,
-            display: pushed[pushed.length - 1]
-          });
-          break;
-        case '-':
-          pushed[z + 1] = (pushed[z - 1] * 1) - (pushed[z + 1] * 1);
-          this.setState({
-            pushed: pushed,
-            display: pushed[pushed.length - 1]
-          });
-          break;
-        case '+':
-          pushed[z + 1] = (pushed[z - 1] * 1) + (pushed[z + 1] * 1);
-          this.setState({
-            pushed: pushed,
-            display: pushed[pushed.length - 1]
-          });
-          break;
-        case '*':
-          pushed[z + 1] = (pushed[z - 1] * 1) / (pushed[z + 1] * 1);
-          this.setState({
-            pushed: pushed,
-            display: pushed[pushed.length - 1]
-          });
-          break;
-        default:
-          break;
-      }
-    }
-    this.setState({
-      pushed: [pushed[pushed.length - 1]]
-    });
+  handlePress() {
+    
   }
 
   render() {
     return (
-      <div className="display">
-        {this.state.display}
-        <Board onClick={(i) => this.handleClick(i)} />
-        <button className="equal" onClick={() => this.calculate()}>
-          =
-        </button>
+      <div>
+        <button onClick={() => this.handlePress()}>{this.state.mode}</button>
+        <Counter name="BREAK" counter={this.state.break} onClick={(n, i) => this.handleClick(n, i)} />
+        <Counter name="SESSION" counter={this.state.session} onClick={(n, i) => this.handleClick(n, i)} />
       </div>
     );
   }
 }
 
-ReactDOM.render(<Calculator />, document.getElementById('root'));
+ReactDOM.render(<PomodoroClock />, document.getElementById('root'));
